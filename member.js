86b61d05313d752ff6b2f80ac11d2f9a2440fe2c
Andrea Peralta Bravo <92412235+aperaltab-tw@users.noreply.github.com>
2024-09-15 23:09:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+  return [
+    {
+      skill: "HTML",
+      point: 10,
+    },
+    {
+      skill: "CSS",
+      point: 9,
+    },
+    {
+      skill: "JavaScript",
+      point: 8,
+    },
+    {
+      skill: "React",
+      point: 7,
+    },
+    {
+      skill: "Node.js",
+      point: 6,
+    },
+    {
+      skill: "MongoDB",
+      point: 5,
+    },
+  ];
+}
